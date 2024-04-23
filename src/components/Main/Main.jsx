f254d7e98960a08001d13cf3ea4d31262cc9f375
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2024-04-23 19:25:40 +0100

    new3

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,31 @@ export const Main = () => {
     <div className='main'>
     <div className='nav'>
     <p>Gemini</p>
-    <img src={assets.user_icon} alt=''/>
+    <img src={assets.paul} alt=''/>
+    </div>
+    <div className='main-container'>
+    <div className='greet'>
+    <p><span>Hello, Paul</span></p>
+    <p>How can I help you today?</p>
+    </div>
+    <div className='cards'>
+    <div className='card'>
+    <p>Suggest Beautiful places to see an upcoming road trip</p>
+    <img src= {assets.compass_icon} alt=''/>
+    </div>
+    <div className='card'>
+    <p>Briefly Summarize this concept: Rural Settlement</p>
+    <img src= {assets.bulb_icon} alt=''/>
+    </div>
+    <div className='card'>
+    <p>Brainstorm team bonding activities for our work retreat</p>
+    <img src= {assets.message_icon} alt=''/>
+    </div>
+    <div className='card'>
+    <p>Improve the readability of the following code</p>
+    <img src= {assets.code_icon} alt=''/>
+    </div>
+    </div>
     </div>
     </div>
   )
