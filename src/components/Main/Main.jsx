39517c98b97ca7d5e4cb5f6b17307a0a6340639c
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2024-04-23 22:16:23 +0100

    new4

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
-
+import paul from '../../assets/paul.jpg'
 export const Main = () => {
   return (
     <div className='main'>
     <div className='nav'>
     <p>Gemini</p>
-    <img src={assets.paul} alt=''/>
+    <img src={paul} alt=''/>
     </div>
     <div className='main-container'>
     <div className='greet'>
@@ -31,6 +31,21 @@ export const Main = () => {
     <p>Improve the readability of the following code</p>
     <img src= {assets.code_icon} alt=''/>
     </div>
+    </div>
+
+    <div className='main-bottom'>
+    <div className='search-box'>
+        <input type='text' placeholder='Enter a prompt here'/>
+        <div>
+           <img src={assets.gallery_icon} alt=''/>
+           <img src={assets.mic_icon} alt=''/>
+           <img src={assets.send_icon} alt=''/>
+        </div>
+    </div>
+    <p className='bottom-info'>
+   Gemini may display inaccurate info, including about people,
+    so double-check its responses. Your privacy and Gemini Apps
+    </p>
     </div>
     </div>
     </div>
